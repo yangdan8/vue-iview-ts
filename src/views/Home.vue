fa87ef972fda7535bae1910cacfe0a3f8fba63c1
yang <yang@139.com>
2018-08-09 00:32:10 +0800

    添加view相关

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,13 +6,14 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Button } from "../iview";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
 export default class Home extends Vue {}
 </script>
