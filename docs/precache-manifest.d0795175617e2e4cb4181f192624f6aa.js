9a16f843f61ca4c45512319011f3e6e73fa18a45
yang <yang@139.com>
2018-09-06 23:21:37 +0800

    添加docs目录发布到github

---
+++ docs/precache-manifest.d0795175617e2e4cb4181f192624f6aa.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3b76c0d2f900aa62d951",
+    "url": "/js/chunk-vendors.3b76c0d2.js"
+  },
+  {
+    "revision": "c8c7da9992b47e9e3812",
+    "url": "/js/app.c8c7da99.js"
+  },
+  {
+    "revision": "05e5c14ddef7b006c120",
+    "url": "/js/about.05e5c14d.js"
+  },
+  {
+    "revision": "460e722320db6cb5d5886ab399575078",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "3b76c0d2f900aa62d951",
+    "url": "/css/chunk-vendors.b75b5709.css"
+  },
+  {
+    "revision": "c8c7da9992b47e9e3812",
+    "url": "/css/app.799de6c0.css"
+  }
+];
